950e02019fcb07fffc4ff0e9a062c03402043229
93094049+kraemr@users.noreply.github.com <93094049+kraemr@users.noreply.github.com>
2024-08-17 02:13:32 +0200

    updated compile.sh

--- examples/compile.sh
+++ examples/compile.sh
@@ -5,3 +5,4 @@ cd examples #return
 gcc exe_to_img.c ../spnglib.so -lz -o exe_to_img -fsanitize=address
 gcc mipmap.c ../spnglib.so -lz -o mipmap -fsanitize=address -lm
 gcc rgb_to_grayscale.c ../spnglib.so -lz -o rgb2grey
+gcc show_info.c ../spnglib.so -lz -o pnginfo
